489082c4c72682711463aca4c8bb2ea2a4130cbb
MRDurgaManiRamaDurga <MRDurgaManiRamaDurga@DESKTOP-CQH7ABK.in.ibm.com>
2019-09-16 16:56:04 +0530

    test

--- src/test1/test2.java
+++ src/test1/test2.java
@@ -6,7 +6,7 @@ public class test2 {
 		
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-		System.out.println("Hello spectra");
+		System.out.println("spectra Testing");
 	}
 
 }
